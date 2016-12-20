1abc1308a3d47f3ff2c5a028cab40a7db66ac275
olegvyacheslavowich <20_ok_13@mail.ru>
2016-12-20 21:21:20 +0600

    Hashcod & equals

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -3,6 +3,14 @@
  */
 public class MainClass {
     public static void main(String[] args){
+        String s = new String ("oelg");
+        String o = new String ("o");
+        if (s.hashCode() == o.hashCode()) System.out.println(s.hashCode() + " = " + o.hashCode());
+      else
+        System.out.println(s.hashCode() + " != " + o.hashCode());
+
+        if (s.equals(o)) System.out.println(s + " = " + o);
+        else System.out.println(s + " != " + o );
 
     }
 }
